eedbe02db3805deae4e8b8cbc27fa2c1ee1b9886
vishalv9505 <vasoyavishu955@gmail.com>
2025-07-17 13:29:46 +0530

    Day 7 of Mission.Api .NET Listing User side operations with Frontent Angular code

---
+++ Day 7/Mission/Mission.Services/IService/IMissionThemeService.cs
@@ -0,0 +1,17 @@
+﻿using Mission.Entities.ViewModels.MissionTheme;
+
+namespace Mission.Services.IService
+{
+    public interface IMissionThemeService
+    {
+        Task<List<MissionThemeViewModel>> GetAllMissionTheme();
+
+        Task<MissionThemeViewModel?> GetMissionThemeById(int missionThemeId);
+
+        Task<bool> AddMissionTheme(MissionThemeViewModel model);
+
+        Task<bool> UpdateMissionTheme(MissionThemeViewModel model);
+
+        Task<bool> DeleteMissionTheme(int missionThemeId);
+    }
+}
